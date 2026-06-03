ff5166fe4a382bfb68e7bc81d12c0309f029768c
santhanalakshmi <119475762+santhanalakshmi04@users.noreply.github.com>
2026-06-03 21:37:16 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,73 @@
+//console.log("hi");
+
+// Variables using var :
+//var name ="Lakshmi"
+//name = "santhana lekshmi"
+//console.log(name)
+
+//let :
+
+//let name = "Ramya"
+//name = "ramya devi"
+//var name = "Javascript"
+//console.log(name)
+
+//const :
+
+//const name = "Lakshmi"
+//name = "Lakshmi k"
+//const name = "santhana lekshmi"
+//console.log(name)
+
+////////////////////////////////////////////////////////
+
+//Data type :
+    //Number - 
+//let age = 21
+//console.log(age)
+//console.log(typeof(age))
+
+// String - 
+//let name = "Lakshmi"
+//console.log(name)
+//console.log(typeof(name))
+
+//boolean -
+
+//let isStudent = true
+//console.log(isStudent)
+//console.log(typeof(isStudent))
+
+//Undefined -
+
+//let city
+//console.log(city)
+//console.log(typeof(city))
+
+//null
+
+//let data = null
+//console.log(data)
+//console.log(typeof(data))
+
+// Object -
+
+//let employee ={
+//    id : 101,
+//  name :"Arjun",
+// salary : 35000
+//}
+
+//console.log(employee)
+//console.log(typeof(employee))
+
+// Array -
+//let colors =["blue","yellow","red"]
+//console.log(colors)
+//console.log(typeof(colors))
+
+//bigInt-
+
+let bigNumber = 12345678901234567890n
+console.log(bigNumber)
+console.log(typeof(bigNumber))
---
+++ Day2.js
@@ -0,0 +1,203 @@
+//Operator
+//Arithmetic operator -
+
+//a = 10
+//b = 20
+//answer = a**b
+//console.log(answer)
+
+//a = 10
+//b=20
+//console.log(a+b)
+//c = a+b
+//console.log(c)
+
+//a = 34
+//b=20
+//console.log(a-b)
+
+//a = 10
+//b = 23
+//console.log(a*b)
+
+//a = 44
+//b = 2
+//console.log(a/b)
+
+//a = 22
+//b = 3
+//console.log(a%b)
+
+//Assignment Operators 
+
+//a = 20
+//b = 36
+//console.log(a+=b)
+
+//a = 20
+//b = 36
+//console.log(a-=b)
+
+//a = 20
+//b = 36
+//console.log(a/=b)
+
+//a = 20
+//b = 36
+//console.log(a*=b)
+
+//a = 20
+//b = 36
+//console.log(a%=b)
+
+//Relational Operator 
+
+//a = 25
+//b = 50
+//console.log(a==b)
+//console.log(a!=b)
+//console.log(a>b)
+//console.log(a<b)
+//console.log(a>=b)
+//console.log(a<=b)
+
+//a = 20
+//b = 20
+//console.log(a===b)
+
+//a = 30
+//b = "30"
+//console.log(a!==b)
+
+//Logical Operator 
+
+//a = 40
+//b = 30
+//console.log(a >b && a !=b && a>=b)
+
+//loan apply - 20 to 45
+//age = 34
+//console.log(age >= 20 && age <= 45)
+
+//a = 88
+//b = 109
+//console.log(a!=b || a>b)
+//console.log(a<b || a<=b)
+//console.log(a==b || a!=b)
+//console.log(a>b || a>=b)
+
+//Not operator
+
+//a = 45
+//b = 45
+//console.log(!a!=b)
+
+//Increment Operator
+
+//a = 34
+//b = a++
+//b=++a
+//console.log(a)
+//console.log(b)
+
+//Decrement Operator
+
+//a=10
+//b=a--
+//b=--a
+//console.log(a)
+//console.log(b)
+
+//conditional statement 
+
+// if
+
+//age = 17
+//if(age >= 18)
+//{
+    //console.log("Eligible to vote")
+//}
+
+//if(age < 18)
+//{
+//    console.log("Not eligible to vote")
+//}
+
+//if..else
+
+//num = 4
+//if(num%2!=0)
+//{
+//    console.log("even")
+//}
+//else{
+//    console.log("odd")
+//}
+
+//Ternary operator
+
+//num = 48
+//answer =(num%2==0) ? "Even Number" : "Odd Number"
+//console.log(answer)
+
+//a = 34
+//b = 99
+//answer =(a>=b) ? "A is greater than b" : "B is greater than a"
+//console.log(answer)
+
+//ladder if...else
+
+//let marks = 48
+//if(marks >= 90)
+//{
+ //   console.log("gradeA+")
+//}
+//else if(marks >= 80)
+//{
+//   console.log("gradeB")
+//}
+//else if(marks >= 60)
+//{
+//    console.log("GradeC")
+//}
+//else if(marks >= 35)
+//{
+//  console.log("GradeD")
+//}
+//else{
+//    console.log("Fail")
+//}
+
+//nested if else
+
+a = 400
+b = 200
+c = 89
+if(a>b)
+{
+    if(a>c){
+        console.log("A is bigg value")
+    }
+    else{
+        console.log("c is bigg value")
+    }
+}
+else{
+    if(b>c)
+    {
+        console.log("B is bigg value")
+    }
+    else{
+        console.log("c is bigg value")
+    }
+}
+
+
+
+
+
+
+
+
+
+
